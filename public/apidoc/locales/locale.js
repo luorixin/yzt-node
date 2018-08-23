7fdd84e9d7deea594e76cc1bd45e10cc844b9c81
Jacob <jacob@i-click.com>
2018-08-23 11:33:59 +0800

    修改路径

--- public/apidoc/locales/locale.js
+++ public/apidoc/locales/locale.js
@@ -1,15 +1,15 @@
 define([
-    'public/apiDoc/locales/ca.js',
-    'public/apiDoc/locales/de.js',
-    'public/apiDoc/locales/es.js',
-    'public/apiDoc/locales/fr.js',
-    'public/apiDoc/locales/it.js',
-    'public/apiDoc/locales/nl.js',
-    'public/apiDoc/locales/pl.js',
-    'public/apiDoc/locales/pt_br.js',
-    'public/apiDoc/locales/ru.js',
-    'public/apiDoc/locales/zh.js',
-    'public/apiDoc/locales/zh_cn.js'
+    'public/apidoc/locales/ca.js',
+    'public/apidoc/locales/de.js',
+    'public/apidoc/locales/es.js',
+    'public/apidoc/locales/fr.js',
+    'public/apidoc/locales/it.js',
+    'public/apidoc/locales/nl.js',
+    'public/apidoc/locales/pl.js',
+    'public/apidoc/locales/pt_br.js',
+    'public/apidoc/locales/ru.js',
+    'public/apidoc/locales/zh.js',
+    'public/apidoc/locales/zh_cn.js'
 ], function() {
     var langId = (navigator.language || navigator.userLanguage).toLowerCase().replace('-', '_');
     var language = langId.substr(0, 2);
